4d8f5efbe767ea794049cb0bf4869d43e48482b3
gozeloglu <gozeloglu@gmail.com>
2020-07-30 23:01:59 +0300

    home page is created

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -2,21 +2,58 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
-  HomePage({Key key}) : super(key : key);
+  HomePage({Key key}) : super(key: key);
 
   @override
   HomeState createState() => HomeState();
 }
 
 class HomeState extends State<HomePage> {
+  List<String> _homePageSelections = <String>[
+    "List Events",
+    "Update Profile",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "11",
+    "12",
+    "13",
+    "14"
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-      padding: EdgeInsets.all(100),
-      child: Text("Home Page"),
-      )
+      appBar: AppBar(
+        elevation: 10,
+        title: Text("Home"),
+        centerTitle: true,
+        backgroundColor: Colors.redAccent,
+        actions: [
+          IconButton(
+            icon: Icon(Icons.person),
+            onPressed: () {},
+          ),
+        ],
+      ),
+      body: ListView.separated(
+        padding: const EdgeInsets.all(10),
+        itemCount: _homePageSelections.length,
+        itemBuilder: (BuildContext context, int index) {
+          return ListTile(
+            title: Text(_homePageSelections[index]),
+            onTap: () {
+              print(index);
+            },
+          );
+        },
+        separatorBuilder: (BuildContext context, int index) => const Divider(),
+      ),
     );
   }
 }
