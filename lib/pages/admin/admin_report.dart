42d9c3b0033f25221ce014cee9ab23d6162d2c14
gozeloglu <gozeloglu@gmail.com>
2020-08-10 17:56:23 +0300

    pie chart is added

---
+++ lib/pages/admin/admin_report.dart
@@ -0,0 +1,123 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:fl_chart/fl_chart.dart';
+import 'indicator.dart';
+
+class AdminReport extends StatefulWidget {
+  AdminReport({Key key, this.registeredUserCount, this.quota})
+      : super(key: key);
+
+  final int registeredUserCount;
+  final int quota;
+
+  AdminReportState createState() => AdminReportState();
+}
+
+class AdminReportState extends State<AdminReport> {
+  int touchedIndex;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 10,
+        title: Text("Pie Chart"),
+        centerTitle: true,
+      ),
+      body: Container(
+        color: Color.fromRGBO(12,12,12,12),
+        child: Card(
+          color: Color.fromRGBO(12, 12, 12, 12),
+          child: Column(
+            children: <Widget>[
+              Expanded(
+                child: PieChart(
+                  PieChartData(
+                      pieTouchData:
+                          PieTouchData(touchCallback: (pieTouchResponse) {
+                        setState(() {
+                          if (pieTouchResponse.touchInput is FlLongPressEnd ||
+                              pieTouchResponse.touchInput is FlPanEnd) {
+                            touchedIndex = -1;
+                          } else {
+                            touchedIndex = pieTouchResponse.touchedSectionIndex;
+                          }
+                        });
+                      }),
+                      borderData: FlBorderData(
+                        show: false,
+                      ),
+                      sectionsSpace: 0,
+                      centerSpaceRadius: 40,
+                      sections: showingSections()),
+                ),
+              ),
+              Column(
+                mainAxisSize: MainAxisSize.max,
+                mainAxisAlignment: MainAxisAlignment.end,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: const <Widget>[
+                  Indicator(
+                    color: Color(0xff0293ee),
+                    text: 'Registered Participant',
+                    isSquare: true,
+                  ),
+                  SizedBox(
+                    height: 8,
+                  ),
+                  Indicator(
+                    color: Color(0xfff8b250),
+                    text: 'Remaining',
+                    isSquare: true,
+                  ),
+                  SizedBox(
+                    height: 16,
+                  ),
+                ],
+              ),
+              const SizedBox(
+                width: 0,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  List<PieChartSectionData> showingSections() {
+    return List.generate(2, (i) {
+      final isTouched = i == touchedIndex;
+      final double fontSize = isTouched ? 25 : 20;
+      final double radius = isTouched ? 60 : 50;
+      switch (i) {
+        case 0:
+          return PieChartSectionData(
+            color: const Color(0xff0293ee),
+            value: widget.registeredUserCount.toDouble(),
+            title: 'Registered: ' + widget.registeredUserCount.toString(),
+            radius: radius,
+            titleStyle: TextStyle(
+                fontSize: fontSize,
+                fontWeight: FontWeight.bold,
+                color: const Color(0xffffffff)),
+          );
+        case 1:
+          return PieChartSectionData(
+            color: const Color(0xfff8b250),
+            value: (widget.quota - widget.registeredUserCount).toDouble(),
+            title: "Remaining: " +
+                (widget.quota - widget.registeredUserCount).toString(),
+            showTitle: true,
+            radius: radius,
+            titleStyle: TextStyle(
+                fontSize: fontSize,
+                fontWeight: FontWeight.bold,
+                color: const Color(0xffffffff)),
+          );
+        default:
+          return null;
+      }
+    });
+  }
+}
---
+++ lib/pages/admin/admin_report_list.dart
@@ -0,0 +1,88 @@
+import 'dart:developer';
+
+import 'package:event_management_app/services/admin_service.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import 'admin_report.dart';
+
+class ReportList extends StatefulWidget {
+  ReportList({Key key}) : super(key: key);
+
+  ReportListState createState() => ReportListState();
+}
+
+class ReportListState extends State<ReportList> {
+  AdminService adminService = new AdminService();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 10,
+        title: Text("Report"),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: FutureBuilder<List<dynamic>>(
+          future: adminService.getAllMeetups(),
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              List<String> meetupName = List();
+              List<String> meetupDetails = List();
+
+              for (int i = 0; i < snapshot.data.length; i++) {
+                meetupName.add(snapshot.data[i]["meetupName"]);
+                meetupDetails.add(snapshot.data[i]["details"]);
+              }
+
+              return ListView.builder(
+                  itemCount: snapshot.data.length,
+                  itemBuilder: (context, index) {
+                    return Card(
+                      shape: new RoundedRectangleBorder(
+                          borderRadius: new BorderRadius.circular(15)),
+                      elevation: 5,
+                      margin: const EdgeInsets.fromLTRB(0, 30, 0, 0),
+                      child: ListTile(
+                        trailing: Icon(Icons.arrow_right),
+                        leading: Icon(Icons.event),
+                        title: Text(meetupName[index]),
+                        subtitle: Text(meetupDetails[index], maxLines: 1),
+                        onTap: () {
+                          Navigator.push(
+                              context,
+                              new MaterialPageRoute(
+                                  builder: (context) => new AdminReport(
+                                      registeredUserCount: snapshot.data[index]
+                                          ["registeredUserCount"],
+                                      quota: snapshot.data[index]["quota"])));
+                        },
+                      ),
+                    );
+                  });
+            } else if (snapshot.data == null) {
+              noDataMessage();
+            }
+            return Container(
+              alignment: Alignment.center,
+              height: 160.0,
+              child: CircularProgressIndicator(),
+            );
+          },
+        ),
+      ),
+    );
+  }
+
+  Widget noDataMessage() {
+    return Center(
+      child: Text(
+        "There is no meetup!",
+        style: TextStyle(
+          fontSize: 20,
+        ),
+      ),
+    );
+  }
+}
